f3046ae47a2907283e19cf9a305d0a15b0197bb9
Astrick <mypiarenoo@gmail.com>
2023-12-22 05:42:28 +0700

    (Fixed): State of input and button in the website

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [greetingMessage, setGreetingMessage] = useState("Greeting Message");
+  const [inputGreetingMessage, setInputGreetingMessage] = useState("");
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{greetingMessage}</div>
       <div className="input-container">
         <label htmlFor="greeting-message">New Greeting Message</label>
-        <input id="greeting-message" type="text" />
+        <input
+          id="greeting-message"
+          type="text"
+          onChange={() => {
+            setInputGreetingMessage(event.target.value);
+          }}
+          value={inputGreetingMessage}
+        />
       </div>
 
       <div className="buttons">
-        <button>Update text</button>
+        <button
+          onClick={() => {
+            setGreetingMessage(inputGreetingMessage);
+          }}
+        >
+          Update text
+        </button>
       </div>
     </div>
   );
